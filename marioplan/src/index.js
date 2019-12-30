1917e2e14a948c9455aa34be31c51b54ffea9216
JennyVong <jennyvong502@gmail.com>
2019-12-30 15:44:53 -0500

    installed firebase and firestore and implemented them to the central store

--- marioplan/src/index.js
+++ marioplan/src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware} from 'redux';
+import { createStore, applyMiddleware, compose} from 'redux';
 import RootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk'
+import { reduxFirestore, getFirestore } from 'redux-firestore'
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
+import firebase from './configurations/fbconfig'
 
-const store = createStore(RootReducer, applyMiddleware(thunk));
+
+const store = createStore(RootReducer,
+    compose ( 
+    applyMiddleware(thunk.withExtraArgument ({getFirestore, getFirebase})),
+    reduxFirestore(firebase),
+    reactReduxFirebase(firebase)
+    )
+    );
+
 
 ReactDOM.render(<Provider store = {store}><App /></Provider>, document.getElementById('root'));
 
