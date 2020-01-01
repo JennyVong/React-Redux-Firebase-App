ed6466eda1d66d90a247861f8965b365bb81c01c
JennyVong <jennyvong502@gmail.com>
2019-12-31 21:18:54 -0500

    connected firestore to dashboard component and project collection

--- marioplan/src/configurations/fbconfig.js
+++ marioplan/src/configurations/fbconfig.js
@@ -12,6 +12,6 @@ var firebaseconfig = {
   };
 
   firebase.initializeApp(firebaseconfig);
-  firebase.firestore().settings({ timestampsInSnapshots: true });
+  firebase.firestore().settings({timestampsInSnapshots: true});
 
   export default firebase;
--- marioplan/src/store/reducers/rootReducer.js
+++ marioplan/src/store/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import authReducer from './authreducer';
 import ProjectReducer from './projectreducer';
 import { combineReducers } from 'redux';
+import { firestoreReducer } from 'redux-firestore'
 
 const RootReducer = combineReducers ({
     auth: authReducer,
-    project: ProjectReducer
+    project: ProjectReducer,
+    firestore: firestoreReducer
 });
 
 export default RootReducer
